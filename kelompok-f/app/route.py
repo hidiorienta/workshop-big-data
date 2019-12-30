3fde1f7c14bb2a01325073ac41d813953f79bf91
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-12-30 17:26:36 +0700

    f: tampil data dari xlsx

--- kelompok-f/app/route.py
+++ kelompok-f/app/route.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request, render_template
 import random
+import os
+import pandas as pd
 
+datas = pd.read_excel(os.path.join(os.getcwd(), '..', 'kelompok-b', 'hasil-kelompok-B.xlsx'))
 router = Blueprint('route', __name__, template_folder='template')
 
 @router.route('/')
@@ -10,4 +13,13 @@ def index():
 @router.route('/search')
 def search():
   keyword = request.args.get('q')
-  return render_template('search.html', keyword=keyword, data=[random.randrange(0, 100) for i in range(3)])
+  sentiments = [0, 0, 0]
+  for idx in datas[datas['2: Tweet'].str.contains(keyword)].index:
+    sentiment = datas['Sentiment'][idx]
+    if sentiment == 'negatif':
+      sentiments[0] += 1
+    elif sentiment == 'netral':
+      sentiments[1] += 1
+    else:
+      sentiments[2] += 1
+  return render_template('search.html', keyword=keyword, data=sentiments)
